c2d05390684f8055afa90f8541df82c78b0757b1
Paul David Mena <paul@extraspecialbitter.com>
2013-06-08 16:34:52 -0400

    reflect reorg in processing scripts

---
+++ bin/process_archive_2007.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+rm -f ./data/archive_2007.html
+./drop_table_2007.rb
+for i in `ls -1 ~/git/haiku_search/data/archive_2007/[0-9]???.html`
+do
+  ./extract_haiku.rb ${i}
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  sed  '/^Paul\ David\ Mena/,$d' qwert.txt > snippet.txt
+  sed  '/^========================/,$d' snippet.txt > qwert.txt
+  sed  '/^------------------------/,$d' qwert.txt > snippet.txt 
+  sed  '/^________________________/,$d' snippet.txt > qwert.txt
+  ./insert_haiku_from_file_into_table_2007.rb qwert.txt
+done
+./haiku_to_html_2007.rb
+rm -rf snippet.txt qwert.txt
---
+++ bin/process_archive_2013.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+rm -f ./data/archive.html
+./drop_table_2013.rb
+for i in `ls -1 ~/git/haiku_search/data/archive/[0-9]???.html`
+do
+  ./extract_haiku_2013.rb ${i}
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  ./insert_haiku_from_file_into_table_2013.rb qwert.txt
+done
+./haiku_to_html_2013.rb
+rm -rf snippet.txt qwert.txt
