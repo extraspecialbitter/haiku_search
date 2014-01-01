75da3d9c160029677af22993c78b7e4774155883
Paul David Mena <pauldavidmena@gmail.com>
2014-01-01 15:31:50 +0000

    add 2014 processing

---
+++ bin/process_archive_2014.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+./drop_table_2014.rb
+for i in `ls -1 ~/git/haiku_search/data/archive/[0-9]???.html`
+do
+  ./extract_haiku_2014.rb ${i}
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+  ./insert_haiku_from_file_into_table_2014.rb qwert.txt
+done
+rm -rf snippet.txt qwert.txt
