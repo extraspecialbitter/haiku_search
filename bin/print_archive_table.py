384bbea5cdd2f07b97f88086cfde7bddf86c3046
Paul David Mena <pmena@constantcontact.com>
2014-04-29 12:50:15 -0400

    now print the rows

--- bin/print_archive_table.py
+++ bin/print_archive_table.py
@@ -32,6 +32,13 @@ cursor = db.cursor()
 # do the actual query
 cursor.execute("SELECT * FROM %s" % table_name)
 
+# fetch all of the rows from the query
+data = cursor.fetchall ()
+
+# print the rows
+for row in data :
+    print row[0], row[1]
+
 # count the rows
 n_rows = cursor.rowcount
 print "There are %s rows in the result set" %n_rows
