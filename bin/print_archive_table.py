deb6a4e615d9d7013a78c728b0c758cb31b436a7
Paul David Mena <pmena@constantcontact.com>
2014-04-29 12:34:45 -0400

    create generic print archive table python script

---
+++ bin/print_archive_table.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+from sys import argv
+import MySQLdb, sys, time
+
+# Validate Argument
+
+arguments = sys.argv[1:]
+count = len(arguments)
+
+if count < 1:
+    print "\n"
+    print "Usage:"
+    print "%s <year>" %sys.argv[0]
+    print "\n"
+    sys.exit(1)
+
+year_string = sys.argv[1]
+if int(year_string) < 1999 or int(year_string) > 2014:
+    print "\n"
+    print "Year must be between 1999 and 2014\n"
+    sys.exit(1)
+
+table_name = "archive_" + year_string
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","menagerie","haiku_archive" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# do the actual query
+cursor.execute("SELECT * FROM %s" % table_name)
+
+# count the rows
+print cursor.rowcount
+    
+# disconnect from server
+db.close()
+
