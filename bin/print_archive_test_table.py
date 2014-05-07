da6cf8c35d840c4ee54ca1e83712b3633b1c3d6b
Paul David Mena <pmena@constantcontact.com>
2014-05-07 10:30:29 -0400

    create a python script to print the archive_test table

---
+++ bin/print_archive_test_table.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+
+from sys import argv
+import MySQLdb, sys, time
+
+table_name = "archive_test"
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","menagerie","haiku_archive" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# do the actual query
+cursor.execute("SELECT * FROM %s" % table_name)
+
+# fetch all of the rows from the query
+data = cursor.fetchall ()
+
+# print the rows
+for row in data :
+    print row[0], row[1]
+
+# count the rows
+n_rows = cursor.rowcount
+print "There are %s rows in the result set" %n_rows
+    
+# disconnect from server
+db.close()
+
