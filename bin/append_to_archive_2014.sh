a3465f644a6caa7253c9d43b1d0fddaa8542c48a
Paul David Mena <paul@extraspecialbitter.com>
2014-05-18 17:39:41 -0400

    now it won't run if it doesn't have anything to process

--- bin/append_to_archive_2014.sh
+++ bin/append_to_archive_2014.sh
@@ -1,19 +1,32 @@
 #!/bin/bash
 
-# see if there's anything new
-rsync -latv --delete mena@sasi.pair.com:/usr/www/users/mena/haikupoet/new_haiku/ /home/pablo/git/haiku_search/data/new_haiku/
+# see if there's anything new to process
 
-# ./drop_haiku_archive_table.py 2014
-for i in `ls -1 ~/git/haiku_search/data/new_haiku/[0-9]???.html`
-do
-  ./extract_haiku_2014.rb ${i}
-  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
-# ./insert_haiku_from_file_into_table_2014.rb qwert.txt
-  ./insert_haiku_from_file_into_table.py 2014 qwert.txt
-done
+rsync -lat --delete mena@sasi.pair.com:/usr/www/users/mena/haikupoet/new_haiku/ /home/pablo/git/haiku_search/data/new_haiku/
+if [ -e /home/pablo/git/haiku_search/data/new_haiku/0000.html ]
+
+# if so append it
+
+then 
+  for i in `ls -1 ~/git/haiku_search/data/new_haiku/[0-9]???.html`
+  do
+    ./extract_haiku_2014.rb ${i}
+    sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+    ./insert_haiku_from_file_into_table.py 2014 qwert.txt
+  done
 
 # cleanup
-rm -rf snippet.txt qwert.txt
-ssh mena@sasi.pair.com "cat /dev/null > /usr/home/mena/mail/new_haiku"
-ssh mena@sasi.pair.com "rm -f /usr/www/users/mena/haikupoet/new_haiku/*"
+
+  rm -rf snippet.txt qwert.txt
+  ssh mena@sasi.pair.com "cat /dev/null > /usr/home/mena/mail/new_haiku"
+  ssh mena@sasi.pair.com "rm -f /usr/www/users/mena/haikupoet/new_haiku/*"
+
+else
+
+# error handling
+
+  echo "nothing new to process"
+  exit 2 
+
+fi
 
