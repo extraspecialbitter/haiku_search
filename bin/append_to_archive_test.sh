0a35864752001f89bda947fc6905893b99a40d5b
Paul David Mena <paul@extraspecialbitter.com>
2014-05-18 14:33:40 -0400

    test of appending code

---
+++ bin/append_to_archive_test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# ./drop_haiku_archive_table.py 2014
+for i in `ls -1 ~/git/haiku_search/data/new_haiku/[0-9]???.html`
+do
+  ./extract_haiku_2014.rb ${i}
+  sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+# ./insert_haiku_from_file_into_table_2014.rb qwert.txt
+  ./insert_haiku_from_file_into_table_test.py 2014 qwert.txt
+done
+# rm -rf snippet.txt qwert.txt
