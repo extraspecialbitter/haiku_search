d8427a8ebb577fbe67f536215ea9efa236aa82bd
Paul David Mena <paul@extraspecialbitter.com>
2014-05-18 14:36:19 -0400

    a few adjustments

--- bin/append_to_archive_test.sh
+++ bin/append_to_archive_test.sh
@@ -6,6 +6,6 @@ do
   ./extract_haiku_2014.rb ${i}
   sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
 # ./insert_haiku_from_file_into_table_2014.rb qwert.txt
-  ./insert_haiku_from_file_into_table_test.py 2014 qwert.txt
+  ./insert_haiku_from_file_into_test_table.py qwert.txt
 done
 # rm -rf snippet.txt qwert.txt
