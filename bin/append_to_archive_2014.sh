b3d7327fd687f92fc31e7856cfd050bdc3165e7e
Paul David Mena <pauldavidmena@gmail.com>
2014-05-20 18:26:10 +0000

    be sure to cd first

--- bin/append_to_archive_2014.sh
+++ bin/append_to_archive_2014.sh
@@ -9,6 +9,7 @@ if [ -e /home/pablo/git/haiku_search/data/new_haiku/0000.html ]
 
 then 
   for i in `ls -1 ~/git/haiku_search/data/new_haiku/[0-9]???.html`
+  cd /home/pablo/git/haiku_search/bin
   do
     ./extract_haiku_2014.rb ${i}
     sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
