3bb9241fef2c80f7d8a1f6192e304121b486efdc
Paul David Mena <paul@extraspecialbitter.com>
2014-10-23 14:46:15 -0400

    prepare database to compare tables

---
+++ bin/prepare_to_compare.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "synching up html archive"
+rsync -lat --delete mena@sasi.pair.com:/usr/home/mena/public_html/haikupoet/archive/ /home/pablo/git/haiku_search/data/archive/
+
+echo "generating a new table from the html archive"
+./process_archive_2014_from_html.sh
+
+echo "grabbing the last MySQL dump"
+scp www.extraspecialbitter.com:~/tmp/archive_2014.sql ~/tmp
+
+echo "sourcing the MySQL dump locally"
+mysql -e "source ~/tmp/archive_2014.sql" haiku_archive
