ebb8c738e4ea600b7ef9ab914ef31602b4cdd81b
Paul David Mena <pauldavidmena@gmail.com>
2014-12-27 22:02:36 +0000

    a few more refinements

--- bin/append_to_archive_2014.sh
+++ bin/append_to_archive_2014.sh
@@ -2,13 +2,13 @@
 
 # see if there's anything new to process
 
-if [ -e /home/pablo/git/haiku_search/data/new_haiku/0000.html ]
+if [ -e /var/www/haikupoet/new_haiku/0000.html ]
 
 # if so append it
 
 then 
   cd /home/pablo/git/haiku_search/bin
-  for i in `ls -1 ~/git/haiku_search/data/new_haiku/[0-9]???.html`
+  for i in `ls -1 /var/www/haikupoet/new_haiku/[0-9]???.html`
   do
     ./extract_haiku_2014.rb ${i}
     sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
