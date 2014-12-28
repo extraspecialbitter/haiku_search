75c1a36f9cdf687a698de0435096e2c8577ed928
Paul David Mena <paul@extraspecialbitter.com>
2014-12-28 14:43:22 -0500

    fix haiku_so_far script

--- bin/haiku_so_far.sh
+++ bin/haiku_so_far.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 export count=0
-for i in `ls -1 print_archive*rb`
+for i in 1999 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014
 do
-  j=`echo $i | cut -d'_' -f3`
-  k=`./${i} | tail -1 | awk '{print $3}'`
-  count=`expr $count + $k`
+  j=`./print_archive_table.py $i | tail -1 | awk '{print $3}'`
+  count=`expr $count + $j`
 done
 echo "$count haiku written so far"
+
