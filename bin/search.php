5c893c7ad9e7b851459a7b450c2fa0ad159f3e0f
Paul David Mena <pauldavidmena@gmail.com>
2015-01-01 16:33:51 +0000

    new stuff for 2015

--- bin/search.php
+++ bin/search.php
@@ -3,7 +3,7 @@
 <html>
 <head>
 <title>search haikupoet.com</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <link href="images/styles.css" rel="stylesheet" type="text/css" />
 </head>
 
@@ -29,22 +29,45 @@
 
 <pre>
 <?php
-$string = $_POST['keywords'];
-$matches = 0;
-$file = fopen("archive.txt", "r") or die("Cannot open file!\n"); 
-while ($line = fgets($file, 1024)) { 
-    if (preg_match("/$string/", $line)) { 
-        echo $line;
-        echo "\n"; 
-        $matches = 1;
-    } 
+$link = mysql_connect('localhost', 'root', 'menagerie');
+if (!$link) {
+    die('Could not connect: ' . mysql_error());
 }
+mysql_select_db('haiku_archive')or die("cannot select db");
+$string = $_POST['keywords'];
+$search_query =    "SELECT haiku_text, date_written FROM archive_1999 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2000 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2001 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2002 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2003 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2004 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2005 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2006 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2007 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2008 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2009 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2010 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2011 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2012 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2013 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2014 WHERE haiku_text LIKE '%$string%' UNION
+                    SELECT haiku_text, date_written FROM archive_2015 WHERE haiku_text LIKE '%$string%'";
+$result = mysql_query($search_query,$link);
+$rows = mysql_num_rows($result);
+if ($rows == 0) {
+    echo "sorry, I haven&#8217;t written a haiku about ".$string." yet.";
+    }
+    $count = 0;
+    while ($count < $rows) {
+        while($row = mysql_fetch_assoc($result)) {
+            echo "{$row['haiku_text']} {$row['date_written']}";
+            echo "\n\n";
+        }
+        $count = $count + 1;
+    }
 
-if ($matches == 0) {
-	echo "sorry, I haven&#8217;t written a haiku about ".$string." yet.";
-} 
+mysql_close($link);
 
-fclose($file); 
 ?>
 </pre>
 
