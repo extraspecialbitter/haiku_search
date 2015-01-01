1060f1f52265c588e79b812433a26a7588508a0a
Paul David Mena <pauldavidmena@gmail.com>
2015-01-01 16:34:41 +0000

    new for 2015

---
+++ bin/append_to_archive_2015.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# see if there's anything new to process
+
+if [ -e /var/www/haikupoet/new_haiku/0000.html ]
+
+# if so append it
+
+then 
+  cd /home/pablo/git/haiku_search/bin
+  for i in `ls -1 /var/www/haikupoet/new_haiku/[0-9]???.html`
+  do
+    ./extract_haiku_2015.rb ${i}
+    sed  '/^Received\ on/,$d' snippet.txt > qwert.txt
+    ./insert_haiku_from_file_into_table.py 2015 qwert.txt
+  done
+
+# cleanup
+
+  rm -rf snippet.txt qwert.txt
+  cat /dev/null > /home/pablo/mail/new_haiku
+  rm -f /var/www/haikupoet/new_haiku/*
+
+else
+
+# error handling
+
+  echo "nothing new to process"
+  exit 2 
+
+fi
+
