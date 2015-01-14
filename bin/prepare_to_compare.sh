6b5298af781a662b409abd67731c0cecc8c61fc7
Paul David Mena <paul@extraspecialbitter.com>
2015-01-14 18:49:10 -0500

    not that it matters but it is accurate

--- bin/prepare_to_compare.sh
+++ bin/prepare_to_compare.sh
@@ -7,7 +7,7 @@ echo "generating a new table from the html archive"
 ./process_archive_2015_from_html.sh
 
 echo "grabbing the last MySQL dump"
-scp www.extraspecialbitter.com:~/tmp/archive_2015.sql ~/tmp
+scp www.haikupoet.com:~/tmp/archive_2015.sql ~/tmp
 
 echo "sourcing the MySQL dump locally"
 mysql -u root -p -e "source ~/tmp/archive_2015.sql" haiku_archive
