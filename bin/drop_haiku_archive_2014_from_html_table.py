e8e5f14ffd401a191b6bfc4ffb26396703c89f23
Paul David Mena <pauldavidmena@gmail.com>
2015-08-23 16:37:37 -0400

    more to enable 2014 processing

---
+++ bin/drop_haiku_archive_2014_from_html_table.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import MySQLdb, sys, time
+
+table_name = "archive_2014_from_html"
+
+# Open database connection
+db = MySQLdb.connect("localhost","root","menagerie","haiku_archive" )
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+# Drop table if it already exist using execute() method.
+cursor.execute("DROP TABLE IF EXISTS %s" % table_name)
+
+# disconnect from server
+db.close()
