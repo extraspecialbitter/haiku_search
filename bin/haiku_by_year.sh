d30d8dc4aa697d91c07a9a7e7c6678f9baf8f381
Paul David Mena <pauldavidmena@gmail.com>
2015-10-18 15:25:38 +0000

    combine haiku_so_far into haiku_by_year

--- bin/haiku_by_year.sh
+++ bin/haiku_by_year.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+export count=0
+
 for i in 1999 2000 2001 2002 2003 2004 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015
 do
   echo -n "${i} : "
-  ./print_archive_table.py $i | tail -1 | awk '{print $3}'
+# ./print_archive_table.py $i | tail -1 | awk '{print $3}'
+
+  j=`./print_archive_table.py $i | tail -1 | awk '{print $3}'`
+  echo ${j}
+  count=`expr $count + $j`
 done
+
+echo " "
+echo "total: ${count}"
