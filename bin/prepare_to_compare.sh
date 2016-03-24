7d5d25844b7d345cdf363f5eb58bc0a91f10dbb9
Paul David Mena <pauldavidmena@gmail.com>
2016-03-24 15:44:58 -0400

    update for 2016

--- bin/prepare_to_compare.sh
+++ bin/prepare_to_compare.sh
@@ -4,10 +4,10 @@ echo "synching up html archive"
 rsync -lat --delete www.haikupoet.com:/var/www/haikupoet/archive/ /home/pablo/git/haiku_search/data/archive/
 
 echo "generating a new table from the html archive"
-./process_archive_2015_from_html.sh
+./process_archive_2016_from_html.sh
 
 echo "grabbing the last MySQL dump"
-scp www.haikupoet.com:~/tmp/archive_2015.sql ~/tmp
+scp www.haikupoet.com:~/tmp/archive_2016.sql ~/tmp
 
 echo "sourcing the MySQL dump locally"
-mysql -u root -p -e "source ~/tmp/archive_2015.sql" haiku_archive
+mysql -u root -p -e "source ~/tmp/archive_2016.sql" haiku_archive
