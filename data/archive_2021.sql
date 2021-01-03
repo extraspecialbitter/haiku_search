59551083c5df915f2f50872b1cfd1dee3cd8db47
Paul David Mena <pauldavidmena@gmail.com>
2021-01-03 12:29:45 -0500

    New Year dump

---
+++ data/archive_2021.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: haiku_archive
+-- ------------------------------------------------------
+-- Server version	5.5.59-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `archive_2021`
+--
+
+DROP TABLE IF EXISTS `archive_2021`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `archive_2021` (
+  `haiku_text` varchar(120) DEFAULT NULL,
+  `date_written` char(22) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `archive_2021`
+--
+
+LOCK TABLES `archive_2021` WRITE;
+/*!40000 ALTER TABLE `archive_2021` DISABLE KEYS */;
+INSERT INTO `archive_2021` VALUES ('New Year&#8217;s morning<br>church bells ringing<br>in my head<br>','<i>1-Jan-2021</i><br>'),('New Year&#8217;s resolutions on a diet<br>','<i>1-Jan-2021</i><br>'),('Buddha resolves<br>to do nothing<br>differently<br>','<i>1-Jan-2021</i><br>'),('telling the squirrels<br>the feeder<br>is for the birds<br>','<i>1-Jan-2021</i><br>'),('all masked up<br>and nowhere to go<br>','<i>1-Jan-2021</i><br>'),('seeking shelter<br>from the cold<br>an origami penguin<br>','<i>1-Jan-2021</i><br>'),('black-eyed peas<br>looking forward<br>to a new year<br>','<i>1-Jan-2021</i><br>'),('a bare wall<br>where last year&#8217;s calendar<br>used to be<br>','<i>1-Jan-2021</i><br>'),('police state<br>blue jays<br>at the bird feeder<br>','<i>2-Jan-2021</i><br>'),('morning clouds<br>the seams<br>in the ceiling<br>','<i>2-Jan-2021</i><br>'),('morning mist<br>it might as well be sunshine<br>to the songbirds<br>','<i>2-Jan-2021</i><br>'),('death and destruction<br>a cattle dog<br>and his toy<br>','<i>2-Jan-2021</i><br>'),('January second<br>a Christmas tree<br>delivered curbside<br>','<i>2-Jan-2021</i><br>'),('mocktails<br>I guess I fail<br>to see the point<br>','<i>2-Jan-2021</i><br>'),('winter wind<br>a stray thread from the afghan<br>Grandma made<br>','<i>2-Jan-2021</i><br>'),('dive bar closed<br>until the twelfth<br>of never<br>','<i>2-Jan-2021</i><br>'),('under cover of darkness<br>someone pretending<br>to be me<br>','<i>2-Jan-2021</i><br>'),('counting the ghosts<br>among the stars<br>before sunrise<br>','<i>2-Jan-2021</i><br>'),('before sunrise<br>counting ghosts<br>among the stars<br>','<i>2-Jan-2021</i><br>'),('before sunrise<br>counting ghosts<br>among stars<br>','<i>2-Jan-2021</i><br>'),('cold Cape wind<br>a parade of hydrangea<br>tumbleweeds<br>','<i>3-Jan-2021</i><br>'),('under cloak of darkness<br>a restless crowd<br>awaiting sunrise<br>','<i>3-Jan-2021</i><br>'),('a new year<br>another day<br>of chasing squirrels<br>','<i>3-Jan-2021</i><br>'),('unable to sleep<br>an unfinished<br>bottle of wine<br>','<i>3-Jan-2021</i><br>'),('midnight<br>her nightgown<br>drops<br>','<i>3-Jan-2021</i><br>'),('strip poker<br>&quot;your move&quot;<br>she says<br>','<i>3-Jan-2021</i><br>');
+/*!40000 ALTER TABLE `archive_2021` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-01-03 12:29:05
