857e69b3b3d2df9b8284720ebe6520b789c8de15
Paul David Mena <pauldavidmena@gmail.com>
2024-01-06 10:17:30 -0500

    Saturday morning dumps

---
+++ data/archive_2024.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: haiku_archive
+-- ------------------------------------------------------
+-- Server version	5.5.59-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `archive_2024`
+--
+
+DROP TABLE IF EXISTS `archive_2024`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `archive_2024` (
+  `haiku_text` varchar(120) DEFAULT NULL,
+  `date_written` char(22) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `archive_2024`
+--
+
+LOCK TABLES `archive_2024` WRITE;
+/*!40000 ALTER TABLE `archive_2024` DISABLE KEYS */;
+INSERT INTO `archive_2024` VALUES ('New Year&#8217;s Day<br>the importance<br>of tildes<br>','<i>1-Jan-2024</i><br>'),('resolutions<br>overflowing trash<br>in the driveway<br>','<i>1-Jan-2024</i><br>'),('New Year&#8217;s Day hangover<br>ignoring the grounds<br>in my coffee<br>','<i>1-Jan-2024</i><br>'),('another year<br>backyard Buddha<br>unmoved<br>','<i>1-Jan-2024</i><br>'),('rock, paper, scissors<br>overcast<br>beats sunshine<br>','<i>1-Jan-2024</i><br>'),('paper tiger<br>the exhaustive research<br>of generative AI<br>','<i>1-Jan-2024</i><br>'),('brave new world<br>a wellness center<br>next to the dispensary<br>','<i>1-Jan-2024</i><br>'),('on the house<br>the bartender thinks<br>I write too much<br>','<i>1-Jan-2024</i><br>'),('just around the bend<br>another poem<br>','<i>1-Jan-2024</i><br>'),('strobe light<br>the illusion<br>of action<br>','<i>1-Jan-2024</i><br>'),('new year<br>fresh graffiti<br>on the subway wall<br>','<i>1-Jan-2024</i><br>'),('spilling the tea<br>the waitress&#8217;s<br>first day<br>','<i>1-Jan-2024</i><br>'),('boundless xenophobia<br>','<i>1-Jan-2024</i><br>'),('the fear of waking up<br>from the wrong dream<br>','<i>1-Jan-2024</i><br>'),('the liminal space<br>between the first and last<br>goodbye<br>','<i>1-Jan-2024</i><br>'),('the noisy little compact<br>that wished<br>it was a sports car<br>','<i>1-Jan-2024</i><br>'),('back to work<br>lichen clinging<br>to an old oak<br>','<i>2-Jan-2024</i><br>'),('out of breath<br>the new train schedule<br>leaves me behind<br>','<i>2-Jan-2024</i><br>'),('first taste of pernil<br>we share<br>a tender moment<br>','<i>2-Jan-2024</i><br>'),('defending<br>rusty jalopies<br>the junkyard dog<br>','<i>2-Jan-2024</i><br>'),('noting the plane&#8217;s exits<br>an illusion<br>of safety<br>','<i>2-Jan-2024</i><br>'),('open mic<br>a pinball machine<br>between poems<br>','<i>2-Jan-2024</i><br>'),('resolutions<br>crows picking at<br>a dead squirrel<br>','<i>2-Jan-2024</i><br>'),('knowing it&#8217;s not his year<br>the rooster crows anyway<br>','<i>2-Jan-2024</i><br>'),('gibbous moon<br>I squint<br>to see it<br>','<i>2-Jan-2024</i><br>'),('dentist convention<br>the guest comedian&#8217;s<br>biting sarcasm<br>','<i>2-Jan-2024</i><br>'),('snow in the forecast<br>the waitress pours me coffee<br>without asking<br>','<i>2-Jan-2024</i><br>'),('slack tide<br>in search of<br>a gentle breeze<br>','<i>2-Jan-2024</i><br>'),('the neighborhood so dark<br>I wonder<br>if it disappeared<br>','<i>2-Jan-2024</i><br>'),('recurring nightmare<br>just in time<br>for someone else&#8217;s train<br>','<i>2-Jan-2024</i><br>'),('a night so dark<br>I fear I might<br>drift away<br>','<i>2-Jan-2024</i><br>'),('lost in the city of dreams<br>','<i>2-Jan-2024</i><br>'),('two voices<br>discussing the river<br>and why it matters<br>','<i>3-Jan-2024</i><br>'),('on the rocks or neat<br>the difference between<br>whiskey and rye<br>','<i>3-Jan-2024</i><br>'),('bridge repair<br>over tranquil waters<br>','<i>3-Jan-2024</i><br>'),('January 3rd<br>a pair of resolutions<br>walks past<br>','<i>3-Jan-2024</i><br>'),('code review<br>watching dry paint<br>peel<br>','<i>3-Jan-2024</i><br>'),('morning dog walk<br>resistance<br>at the end of the leash<br>','<i>3-Jan-2024</i><br>'),('street guitarist<br>the arpeggio<br>of rain<br>','<i>3-Jan-2024</i><br>'),('spoiled pigeons<br>refusing crumbs<br>of stale bread<br>','<i>3-Jan-2024</i><br>'),('bus stop busker<br>loose joints<br>in his tip jar<br>','<i>3-Jan-2024</i><br>'),('presidential debate<br>a ripe quince falls<br>between two squirrels<br>','<i>3-Jan-2024</i><br>'),('returned to sender<br>a bundle<br>of nerves<br>','<i>3-Jan-2024</i><br>'),('dawdling<br>winter mist<br>','<i>3-Jan-2024</i><br>'),('souvenir<br>from a business trip<br>a positive COVID test<br>','<i>3-Jan-2024</i><br>'),('this damnably faint praise<br>','<i>3-Jan-2024</i><br>'),('the wind picks up<br>remembering where I hid<br>that last gift<br>','<i>3-Jan-2024</i><br>'),('a tree full of robins<br>maybe I&#8217;m the one<br>who&#8217;s lost<br>','<i>3-Jan-2024</i><br>'),('perhaps I&#8217;ll wear them<br>one more time<br>my paisley ties<br>','<i>4-Jan-2024</i><br>'),('aftershocks<br>a rain puddle<br>trembles<br>','<i>4-Jan-2024</i><br>'),('building a hangover<br>construction workers<br>at the bar<br>','<i>4-Jan-2024</i><br>'),('startled from sleep<br>I invent<br>a new language<br>','<i>4-Jan-2024</i><br>'),('receding waves<br>a part of me<br>dissolves<br>','<i>4-Jan-2024</i><br>'),('backstage pass<br>we remain<br>invisible<br>','<i>4-Jan-2024</i><br>'),('winter storm watch<br>how many pronounce the &quot;h&quot;<br>in &quot;historic&quot;?<br>','<i>4-Jan-2024</i><br>'),('winter storm watch<br>pronouncing the &quot;h&quot;<br>in &quot;historic&quot;<br>','<i>4-Jan-2024</i><br>'),('sleepwalking<br>the snow<br>has yet to fall<br>','<i>4-Jan-2024</i><br>'),('hanging from the rim<br>of a baseball net<br>icicles<br>','<i>4-Jan-2024</i><br>'),('in place of a Christmas tree<br>the pot plant<br>in our neighbor&#8217;s backyard<br>','<i>5-Jan-2024</i><br>'),('passing the cemetery<br>a parking lot filled<br>with unsold Christmas trees<br>','<i>5-Jan-2024</i><br>'),('town landfill<br>a flatbed filled<br>with unsold Christmas trees<br>','<i>5-Jan-2024</i><br>'),('moving on<br>the ultimate<br>sacrifice<br>','<i>5-Jan-2024</i><br>'),('Big Bang<br>my waistline<br>expands<br>','<i>5-Jan-2024</i><br>'),('surface tension<br>dreams of walking<br>on water<br>','<i>5-Jan-2024</i><br>'),('Wall Street pub<br>buying another round<br>of layoffs<br>','<i>5-Jan-2024</i><br>'),('night owl<br>a light on<br>across the river<br>','<i>5-Jan-2024</i><br>'),('swirling in the dark<br>the reflectors<br>on a bike pedal<br>','<i>5-Jan-2024</i><br>'),('leaving in a hurry<br>our neighbor&#8217;s pumpkin<br>left to rot<br>','<i>5-Jan-2024</i><br>'),('street magician<br>my wallet<br>disappears<br>','<i>5-Jan-2024</i><br>'),('in remembrance<br>I mistakenly call them<br>mass cards<br>','<i>5-Jan-2024</i><br>'),('Civil War monument<br>a peaceful protest<br>turns violent<br>','<i>5-Jan-2024</i><br>'),('heavy traffic<br>the supermarket<br>before a storm<br>','<i>5-Jan-2024</i><br>'),('winter storm watch<br>flickering lights<br>in the indoor pool<br>','<i>5-Jan-2024</i><br>'),('dry January<br>ranch water<br>without the ranch<br>','<i>5-Jan-2024</i><br>'),('gale warning<br>the night the moon<br>stared back<br>','<i>5-Jan-2024</i><br>'),('unable to sleep<br>unable to stay awake<br>the howling silence<br>','<i>5-Jan-2024</i><br>'),('mixed precipitation<br>I mock<br>my mocktail<br>','<i>6-Jan-2024</i><br>'),('the eyes in the trailcam<br>may be wearier<br>than they appear<br>','<i>6-Jan-2024</i><br>');
+/*!40000 ALTER TABLE `archive_2024` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-06 10:16:56
