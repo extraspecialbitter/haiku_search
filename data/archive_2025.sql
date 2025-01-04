1050bb9ebc30cbc95ad881a030e65894dd6e8b67
Paul David Mena <pauldavidmena@gmail.com>
2025-01-04 13:14:25 -0500

    Saturday afternoon dumps

---
+++ data/archive_2025.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: haiku_archive
+-- ------------------------------------------------------
+-- Server version	5.5.59-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `archive_2025`
+--
+
+DROP TABLE IF EXISTS `archive_2025`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `archive_2025` (
+  `haiku_text` varchar(120) DEFAULT NULL,
+  `date_written` char(22) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `archive_2025`
+--
+
+LOCK TABLES `archive_2025` WRITE;
+/*!40000 ALTER TABLE `archive_2025` DISABLE KEYS */;
+INSERT INTO `archive_2025` VALUES ('New Year&#8217;s Day<br>let the cleanup<br>begin<br>','<i>1-Jan-2025</i><br>'),('actors&#8217; retreat<br>the street mime&#8217;s<br>life story<br>','<i>1-Jan-2025</i><br>'),('Season&#8217;s Greetings<br>a Christmas tree<br>at the curb<br>','<i>1-Jan-2025</i><br>'),('new year goals<br>a 365-day<br>Wordle streak<br>','<i>1-Jan-2025</i><br>'),('New Year&#8217;s Day<br>some assembly<br>required<br>','<i>1-Jan-2025</i><br>'),('actors&#8217; retreat<br>a street mime shares<br>his life stor<br>','<i>1-Jan-2025</i><br>'),('it&#8217;s beginning to feel a lot like Doomsday<br>','<i>1-Jan-2025</i><br>'),('text notification<br>hope is back-ordered<br>due to supply chain issues<br>','<i>1-Jan-2025</i><br>'),('new year<br>same shopping cart<br>with a broken wheel<br>','<i>1-Jan-2025</i><br>'),('town landfill<br>seagulls dining<br>Ã  la carte<br>','<i>1-Jan-2025</i><br>'),('the ghost&#8217;s<br>leisurely stroll<br>through my nightmare<br>','<i>1-Jan-2025</i><br>'),('the starting roster<br>everything old<br>is new again<br>','<i>1-Jan-2025</i><br>'),('security breach<br>a password<br>on the whiteboard<br>','<i>1-Jan-2025</i><br>'),('a new year<br>of high-resolution<br>photography<br>','<i>1-Jan-2025</i><br>'),('street preacher<br>salvation<br>and a cup of coffee<br>','<i>1-Jan-2025</i><br>'),('November windstorm<br>election signs<br>vote to secede<br>','<i>1-Jan-2025</i><br>'),('the joy of snowfall somewhere else<br>','<i>1-Jan-2025</i><br>'),('toasting another sober January<br>','<i>1-Jan-2025</i><br>'),('21st century dream<br>kissing a robot<br>expecting a princess<br>','<i>1-Jan-2025</i><br>'),('broken clouds<br>a night<br>of wandering stars<br>','<i>1-Jan-2025</i><br>'),('tomorrow another box on the calendar<br>','<i>2-Jan-2025</i><br>'),('winter wind<br>how it strikes<br>a triangle<br>','<i>2-Jan-2025</i><br>'),('wondering<br>if one can be disqualified<br>from the human race<br>','<i>2-Jan-2025</i><br>'),('rain in the forecast<br>the river says<br>&quot;hold my beer&quot;<br>','<i>2-Jan-2025</i><br>'),('old enough to have used a slide rule<br>','<i>2-Jan-2025</i><br>'),('new year<br>forgetting the words<br>to &quot;Auld Lang Syne&quot;<br>','<i>2-Jan-2025</i><br>'),('wild turkeys<br>how a bill<br>becomes a law<br>','<i>2-Jan-2025</i><br>'),('waiting for the bill<br>a seagull finishes<br>my french fries<br>','<i>2-Jan-2025</i><br>'),('Naked Cowboy<br>a modest rally<br>on Wall Street<br>','<i>2-Jan-2025</i><br>'),('back to work<br>fresh graffiti<br>at the train station<br>','<i>2-Jan-2025</i><br>'),('whiskey tango foxtrot<br>every time<br>I read the news<br>','<i>2-Jan-2025</i><br>'),('a resolution:<br>continued<br>imperfection<br>','<i>2-Jan-2025</i><br>'),('reflected<br>in my unblinking eyes<br>the last train home<br>','<i>2-Jan-2025</i><br>'),('waxing crescent moon<br>I guess it&#8217;s time<br>to move on<br>','<i>2-Jan-2025</i><br>'),('pouring the wine<br>withering side-eye<br>from the Fun Police<br>','<i>2-Jan-2025</i><br>'),('wanted:<br>a less complicated life<br>','<i>2-Jan-2025</i><br>'),('se busca:<br>una vida<br>sin complejos<br>','<i>2-Jan-2025</i><br>'),('ghost gear<br>haunting<br>the harbor<br>','<i>2-Jan-2025</i><br>'),('sickle moon<br>a one-eyed dog<br>','<i>2-Jan-2025</i><br>'),('spider web<br>keeping the stars<br>in place<br>','<i>2-Jan-2025</i><br>'),('the little lost boy<br>who finally found<br>nowhere<br>','<i>2-Jan-2025</i><br>'),('searching for my dream<br>and finding<br>only feathers<br>','<i>2-Jan-2025</i><br>'),('cold front<br>suddenly the ads<br>for assisted living<br>','<i>2-Jan-2025</i><br>'),('and to think<br>I never said<br>&quot;wait till next year&quot;<br>','<i>2-Jan-2025</i><br>'),('ghost dog<br>still waiting to be called<br>a good boy<br>','<i>2-Jan-2025</i><br>'),('finding peace<br>or perhaps only darkness<br>beneath the ice<br>','<i>3-Jan-2025</i><br>'),('progress?<br>it&#8217;s not the angriest<br>I&#8217;ve ever been<br>','<i>3-Jan-2025</i><br>'),('seasons of change<br>an updated profile<br>on LinkedIn<br>','<i>3-Jan-2025</i><br>'),('express train<br>the countryside<br>races by<br>','<i>3-Jan-2025</i><br>'),('a breeze so comfortable<br>it doesn&#8217;t want to leave<br>','<i>3-Jan-2025</i><br>'),('year of the snake<br>the rooster crows anyway<br>','<i>3-Jan-2025</i><br>'),('smooth segue<br>between the snoring dog<br>and a pale sky<br>','<i>3-Jan-2025</i><br>'),('a vulture tells me<br>the bus doesn&#8217;t<br>stop here anymore<br>','<i>3-Jan-2025</i><br>'),('old mill town<br>a stop sign riddled<br>with bullet holes<br>','<i>3-Jan-2025</i><br>'),('deeply moving<br>a robot arm opens<br>the lobster trap<br>','<i>3-Jan-2025</i><br>'),('resolution --<br>to build<br>a better mousetrap<br>','<i>3-Jan-2025</i><br>'),('indistinct voices<br>lowering a burning rope<br>into the abyss<br>','<i>3-Jan-2025</i><br>'),('budget cuts<br>a seagull<br>in the lifeguard chair<br>','<i>3-Jan-2025</i><br>'),('turning back time<br>the gavel<br>of a reactionary judge<br>','<i>3-Jan-2025</i><br>'),('penguin dust<br>leaning against<br>a crooked tombstone<br>','<i>3-Jan-2025</i><br>'),('early morning coffee<br>the rooster crows<br>within me<br>','<i>3-Jan-2025</i><br>'),('sickle moon<br>its sly wink<br>','<i>3-Jan-2025</i><br>'),('playful hazing<br>I miss my train<br>and dinner awaits<br>','<i>4-Jan-2025</i><br>'),('Christmas decorations<br>pondering the case<br>for deportation<br>','<i>4-Jan-2025</i><br>'),('musings<br>from the other side<br>of an unfinished wall<br>','<i>4-Jan-2025</i><br>'),('calming signals<br>the dog yawns<br>when I mention them<br>','<i>4-Jan-2025</i><br>'),('mindful meltdowns<br>and other myths<br>of so-called enlightenment<br>','<i>4-Jan-2025</i><br>'),('oversleeping<br>I miss the wake<br>of a former colleague<br>','<i>4-Jan-2025</i><br>'),('clearance sale<br>an inflatable reindeer<br>in the original packaging<br>','<i>4-Jan-2025</i><br>'),('cold enough to kill a perennial<br>','<i>4-Jan-2025</i><br>'),('lunch break<br>the delivery man<br>refills his flask<br>','<i>4-Jan-2025</i><br>'),('strapped to the car roof<br>a Christmas tree<br>in January<br>','<i>4-Jan-2025</i><br>'),('no strings attached<br>she loosens<br>her shoulder straps<br>','<i>4-Jan-2025</i><br>');
+/*!40000 ALTER TABLE `archive_2025` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-04 13:13:50
