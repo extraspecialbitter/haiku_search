935131d1f9e9a89f528ace68fd37f027c94b2df3
Paul David Mena <pauldavidmena@gmail.com>
2026-01-05 12:40:55 -0500

    Monday afternoon adds

---
+++ data/archive_2026.sql
@@ -0,0 +1,50 @@
+-- MySQL dump 10.13  Distrib 5.5.59, for debian-linux-gnu (x86_64)
+--
+-- Host: localhost    Database: haiku_archive
+-- ------------------------------------------------------
+-- Server version	5.5.59-0+deb7u1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `archive_2026`
+--
+
+DROP TABLE IF EXISTS `archive_2026`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `archive_2026` (
+  `haiku_text` varchar(120) DEFAULT NULL,
+  `date_written` char(22) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `archive_2026`
+--
+
+LOCK TABLES `archive_2026` WRITE;
+/*!40000 ALTER TABLE `archive_2026` DISABLE KEYS */;
+INSERT INTO `archive_2026` VALUES ('Buddha&#8217;s new wardrobe<br>just a passing trend<br>','<i>1-Jan-2026</i><br>'),('first sunrise<br>looking an awful lot<br>like snow<br>','<i>1-Jan-2026</i><br>'),('cozy campfire<br>we now return<br>to growing old<br>','<i>1-Jan-2026</i><br>'),('television off<br>the year changes anyway<br>','<i>1-Jan-2026</i><br>'),('kite flyer<br>hanging<br>by a thread<br>','<i>1-Jan-2026</i><br>'),('virtuous signaling<br>I let the old woman<br>cut ahead of me<br>','<i>1-Jan-2026</i><br>'),('what a mess!<br>I try to saber<br>a champagne bottle<br>','<i>1-Jan-2026</i><br>'),('champagne taste<br>on a Night Train budget<br>','<i>1-Jan-2026</i><br>'),('crows chasing daylight<br>','<i>1-Jan-2026</i><br>'),('laughing at Grandpa<br>when he steps on a Lego<br>my sadistic grandson<br>','<i>1-Jan-2026</i><br>'),('sheer delight<br>she shops<br>for lingerie<br>','<i>1-Jan-2026</i><br>'),('sheer delight<br>watching her shop<br>for lingerie<br>','<i>1-Jan-2026</i><br>'),('used record store<br>Clam Dip<br>& Other Delights<br>','<i>1-Jan-2026</i><br>'),('sheer delight<br>pretending<br>she doesn&#8217;t know<br>','<i>1-Jan-2026</i><br>'),('fortune cookie<br>an unencrypted<br>text message<br>','<i>1-Jan-2026</i><br>'),('unseasonable weather<br>no idea<br>how to dress for it<br>','<i>1-Jan-2026</i><br>'),('seasonably cold<br>the girl who never<br>says hello<br>','<i>1-Jan-2026</i><br>'),('unhappy hour<br>a cold shoulder<br>at the bar<br>','<i>1-Jan-2026</i><br>'),('finding my heart<br>a lost dog<br>','<i>1-Jan-2026</i><br>'),('typo in my hashtag<br>I invent a new genre<br>of poetry<br>','<i>1-Jan-2026</i><br>'),('celebration<br>one more year<br>to live in the past<br>','<i>1-Jan-2026</i><br>'),('resolution<br>another secret<br>kept<br>','<i>1-Jan-2026</i><br>'),('first snow<br>the champagne glasses<br>put away<br>','<i>2-Jan-2026</i><br>'),('urban renewal<br>another truckload<br>of cinder blocks<br>','<i>2-Jan-2026</i><br>'),('competing<br>for prime real estate<br>piping plovers<br>','<i>2-Jan-2026</i><br>'),('continuous cycle<br>the rally<br>finances itself<br>','<i>2-Jan-2026</i><br>'),('irregardless of correct grammar<br>','<i>2-Jan-2026</i><br>'),('regarding masks<br>the ones he wears<br>are invisible<br>','<i>2-Jan-2026</i><br>'),('hydrangeas in bloom<br>the prospect makes<br>his big league debut<br>','<i>2-Jan-2026</i><br>'),('abstract impressionism<br>an AI-generated<br>hallucination<br>','<i>2-Jan-2026</i><br>'),('not waiting<br>for the walk signal<br>wolf moon<br>','<i>2-Jan-2026</i><br>'),('World Introvert Day<br>I celebrate it<br>alone<br>','<i>2-Jan-2026</i><br>'),('Labyrinth of Solitude<br>lost in a crowd<br>at IKEA<br>','<i>2-Jan-2026</i><br>'),('wolf moon<br>my unshaved face<br>','<i>2-Jan-2026</i><br>'),('today&#8217;s headlines<br>a despot&#8217;s<br>hollow laughter<br>','<i>3-Jan-2026</i><br>'),('today&#8217;s headlines<br>a crime drama<br>disguised as news<br>','<i>3-Jan-2026</i><br>'),('new year<br>more of the same<br>bitter angst<br>','<i>3-Jan-2026</i><br>'),('downtown Caracas<br>waking to sirens<br>and shuttered shops<br>','<i>3-Jan-2026</i><br>'),('dirty snow<br>my resolution<br>to remain apolitcal<br>','<i>3-Jan-2026</i><br>'),('deserted beach<br>conflating silence<br>with peace<br>','<i>3-Jan-2026</i><br>'),('one beer too many<br>the ceiling fan<br>wobbles<br>','<i>3-Jan-2026</i><br>'),('reviewing the evidence<br>I draw<br>a conclusion<br>','<i>3-Jan-2026</i><br>'),('AI-generated cartoonist<br>drawing a blank<br>','<i>3-Jan-2026</i><br>'),('Koreatown<br>all signs<br>pointing east<br>','<i>3-Jan-2026</i><br>'),('bootlickers in praise of balderdash<br>','<i>3-Jan-2026</i><br>'),('in the mirror<br>an honors student<br>who never raised his hand<br>','<i>3-Jan-2026</i><br>'),('holiday buffet<br>I eat<br>my feelings<br>','<i>3-Jan-2026</i><br>'),('fortune teller unemployed<br>','<i>3-Jan-2026</i><br>'),('home from school<br>my granddaughter&#8217;s<br>bag of laundry<br>','<i>3-Jan-2026</i><br>'),('dirty snow<br>taking down<br>the Christmas lights<br>','<i>3-Jan-2026</i><br>'),('wolf moon<br>airspace closed<br>to protect plundered oil<br>','<i>3-Jan-2026</i><br>'),('wild poppies<br>a Red Heeler<br>playing in the snow<br>','<i>3-Jan-2026</i><br>'),('snow moon<br>I can&#8217;t<br>let go<br>','<i>3-Jan-2026</i><br>'),('wolf moon<br>a veil<br>over suburbia<br>','<i>3-Jan-2026</i><br>'),('wolf moon rising<br>a fox<br>crosses the street<br>','<i>4-Jan-2026</i><br>'),('pickled onions<br>they go<br>with everything<br>','<i>4-Jan-2026</i><br>'),('first day of retirement<br>my alarm stares at me<br>in alarm<br>','<i>4-Jan-2026</i><br>'),('recurring nightmare<br>the cab driver won&#8217;t tell us<br>where we&#8217;re going<br>','<i>4-Jan-2026</i><br>'),('bear market<br>wagging the dog<br>','<i>4-Jan-2026</i><br>'),('non-alcoholic bourbon<br>the pointlessness<br>of everything<br>','<i>4-Jan-2026</i><br>'),('breaking my fall<br>a cold hard cider<br>','<i>4-Jan-2026</i><br>'),('everywhere I turn a lost dog<br>','<i>4-Jan-2026</i><br>'),('the planet&#8217;s last pay phone<br>ringing unanswered<br>','<i>4-Jan-2026</i><br>'),('Caribbean daydream<br>I turn into<br>a tree frog<br>','<i>4-Jan-2026</i><br>'),('erotic daydream<br>please tell me<br>it&#8217;s not AI<br>','<i>4-Jan-2026</i><br>'),('finally an algorithm<br>ready for prime time<br>','<i>4-Jan-2026</i><br>');
+/*!40000 ALTER TABLE `archive_2026` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-01-04 13:32:35
