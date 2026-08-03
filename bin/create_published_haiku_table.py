c19bada7d2bf169e8b9edca8d0241fb406b8d6dc
Paul David Mena <pauldavidmena@gmail.com>
2026-08-03 12:31:01 -0400

    expand the table definition

--- bin/create_published_haiku_table.py
+++ bin/create_published_haiku_table.py
@@ -12,8 +12,13 @@ cursor = db.cursor()
 # Create table using execute() method.
 
 sql = """CREATE TABLE IF NOT EXISTS published_haiku (
+         index INT,
          haiku_text VARCHAR(120),
-         date_written CHAR(22))"""
+         publication_name CHAR(22),
+         year CHAR(6),
+         month CHAR(6),
+         volume CHAR(12),
+         issue CHAR(12))"""
 
 cursor.execute(sql)
 
